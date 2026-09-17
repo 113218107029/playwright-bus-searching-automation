f1a5be675fd8afabf9ea856b02db17fbf59ba0fa
Revathi Thamizharasan <revathithamizharasan24@gmail.com>
2026-09-17 17:08:19 +0530

    Fixed Playwright headless configuration

--- playwright.config.js
+++ playwright.config.js
@@ -39,7 +39,7 @@ export default defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'],
-           headless: !process.env.CI,
+           headless: true,
        },
    
     },
